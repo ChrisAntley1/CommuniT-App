ad63518299303bf1d8c52b553a3631e0f541fcfe
Crantly1 <77400072+Crantly1@users.noreply.github.com>
2021-03-13 16:59:24 -0500

    created communities xml page and activity

--- app/src/main/java/com/example/emergencyapp/CommunitiesActivity.java
+++ app/src/main/java/com/example/emergencyapp/CommunitiesActivity.java
@@ -1,8 +1,10 @@
 package com.example.emergencyapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
@@ -15,5 +17,28 @@ public class CommunitiesActivity extends Activity{
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_communities);
+
+        churchCard = findViewById(R.id.churchCard);
+        streetCard = findViewById(R.id.streetCard);
+
+        churchCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CommunitiesActivity.this, MainActivity.class));
+                Toast.makeText(CommunitiesActivity.this, "Showing plans for Oak Hill Church Community",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        streetCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CommunitiesActivity.this, MainActivity.class));
+                Toast.makeText(CommunitiesActivity.this, "Showing plans for Mainstreet Community",
+                        Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/emergencyapp/MainActivity.java
+++ app/src/main/java/com/example/emergencyapp/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends Activity {
         communitiesCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CommunitiesActivity.class));
             }
         });
 
