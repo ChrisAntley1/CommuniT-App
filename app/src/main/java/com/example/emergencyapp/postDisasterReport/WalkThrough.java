6c861400321b857d6e68470868cd43c9005d5735
Crantly1 <77400072+Crantly1@users.noreply.github.com>
2021-04-08 10:56:52 -0400

    Began work on post disaster report. Created framework pages for each step of the report.

---
+++ app/src/main/java/com/example/emergencyapp/postDisasterReport/WalkThrough.java
@@ -0,0 +1,16 @@
+package com.example.emergencyapp.postDisasterReport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.emergencyapp.R;
+
+public class WalkThrough extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_walk_through);
+    }
+}
